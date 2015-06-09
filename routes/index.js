a63eab050f9d8855facc605c916aea331db993d9
Juan Carlos Sierra Jiménez <jcsj1172@gmail.com>
2015-06-09 23:31:47 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+//añadimos el acceso a quiz_controlers
+var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+//metodo get para cuestiones y respuestas de quizController
+router.get('/quizes/question',quizController.question);
+router.get('/quizes/answer',quizController.answer);
 
 module.exports = router;
